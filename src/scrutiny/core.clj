9d37df36a3bc0dec79bc82f62ce0aab557038fdf
cursivecode <cursivecode@gmail.com>
2014-04-22 06:12:06 -0400

    Change program design & finish functions

--- src/scrutiny/core.clj
+++ src/scrutiny/core.clj
@@ -10,94 +10,91 @@
    :nickname "joe dirt"})
 
 (defn result
-  "returns the map or map with scrutiny message based on result."
-  [m good-validation msg]
-  (if good-validation
+  "Returns the map or map with scrutiny message based on result."
+  [m msg validation]
+  (if validation
     m
     (conj m {:scrutiny msg})))
 
-(defn present-helper
-  "checks if key is present"
-  [m k]
-  (if (m :scrutiny)
+(defn scrutinize
+  ""
+  [m f]
+  (if (:scrutiny m)
     m
-    (result m
-            (not= false (get m k false))
-            (str k " is not present"))))
+    (let [{:keys [msg pred]} (f)]
+      (result m msg pred))))
 
-(defn present
-  "reduce over keys calling present-helper"
-  [m & kys]
+(defn present-helper
+  ""
+  [m k]
+  (scrutinize m #(hash-map :msg (str k " is not present")
+                           :pred (not= :scrfalse (get m k :scrfalse)))))
+
+(defn present [m & kys]
   (reduce present-helper m kys))
 
 (defn required-helper
-  ""
+  "Checks if k is empty or nil"
   [m k]
-  (if (m :scrutiny)
-    m
-    (result m
-            (not (or (empty? (m k))
-                     (nil? (m k))))
-            (str k " was empty or nil"))))
+  (scrutinize m #(hash-map :msg (str k " was empty or nil")
+                           :pred (not (or (empty? (k m))
+                                          (nil? (k m)))))))
 
-(defn required
-  ""
-  [m & kys]
+(defn required [m & kys]
   (reduce required-helper m kys))
 
-(defn confirmation
-  "Check to see if key matches another key"
-  [m k k2]
-  (if (m :scrutiny)
-    m
-    (result m
-            (= (m k) (m k2))
-            (str k " and " k2 " do not match"))))
+(defn confirmation-helper
+  "Check to see if key equals another key"
+  [m [k k2]]
+  (scrutinize m #(hash-map :msg (str k " and " k2 " do not match")
+                           :pred (= (k m) (k2 m)))))
 
-(defn length
-  ""
-  [m f k]
-  (if (m :scrutiny)
-    m
-    (result m
-            (f (m k))
-            (str k " failed length function"))))
+(defn confirmation [m & kys]
+  {:pre [(even? (count kys))]}
+  (reduce confirmation-helper m (partition 2 kys)))
 
-(defn inclusion
+(defn length-helper
+  "Checks result of applying f to value."
+  [f m k]
+  (scrutinize m #(hash-map :msg (str k " failed length function")
+                           :pred (f (k m)))))
+
+(defn length [m f & kys]
+  (reduce (partial length-helper f) m kys))
+
+(defn inclusion-helper
   "Checks to see if key returns a truthy value from function"
-  [m f k]
-  (if (m :scrutiny)
-    m
-    (result m
-            (f (m k))
-            (str k " failed inclusion function"))))
+  [f m k]
+  (scrutinize m #(hash-map :msg (str k " failed inclusion function")
+                           :pred (f (k m)))))
 
-(defn exclusion
+(defn inclusion [m f & kys]
+  (reduce (partial inclusion-helper f) m kys))
+
+(defn exclusion-helper
   "Checks to see if key returns a truthy value from function"
-  [m f k]
-  (if (m :scrutiny)
-    m
-    (result m
-            (not (f (m k)))
-            (str k " failed exclusion function"))))
+  [f m k]
+  (scrutinize #(hash-map :msg (str k " failed exclusion function")
+                         :pred (not (f (k m))))))
 
-(defn match
-  ""
-  [m regex k]
-  (if (m :scrutiny)
-    m
-    (result m
-            (re-find regex (str (m k)))
-            (str k " failed match function"))))
+(defn exclusion [m f & kys]
+  (reduce (partial exclusion-helper f) m kys))
+
+(defn match-helper
+  "Checks to see if regex expression finds anything."
+  [regex m k]
+  (scrutinize m #(hash-map :msg (str k " failed match function")
+                           :pred (re-find regex (str (k m))))))
+
+(defn match [m regex & kys]
+  (reduce (partial match-helper regex) m kys))
 
 (defn custom-helper
-  ""
+  "Checks to see if function returns a truthy value.
+   Also applies custom function message."
   [msg f m k]
-  (if (m :scrutiny)
-    m
-    (result m
-            (f (m k))
-            msg)))
+  (scrutinize m #(hash-map :msg msg
+                           :pred (f (m k)))))
 
 (defn custom
   [m f msg & kys]
@@ -106,10 +103,11 @@
 
 (-> test-map
     (present :password :gender :confirmation)
-    (confirmation :password :confirmation)
-    (inclusion #(some #{%} (range 0 46)) :age)
+    (confirmation :password :confirmation :username :username)
+    (inclusion #(some #{%} (range 18 46)) :age)
     (match #"[a-zA-Z]" :password)
-    (length #(> (count %) 2) :username)
+    (length #(and (< (count %) 15)
+                  (> (count %) 2)) :username :password)
     (required :password :confirmation)
     (custom #(number? %) "Age must be a number" :age)
     )
