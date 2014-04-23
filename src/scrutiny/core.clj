ff0dd6afe11a210a127925ae30e68d34f82e5e6f
cursivecode <cursivecode@gmail.com>
2014-04-23 02:41:45 -0400

    Change design

--- src/scrutiny/core.clj
+++ src/scrutiny/core.clj
@@ -1,45 +1,49 @@
-(ns scrutiny.core)
-
-(defn result
-  "Returns the map or map with scrutiny message based on result."
-  [m msg validation]
-  (if validation
-    m
-    (conj m {:scrutiny msg})))
+(ns scrutiny.core
+  (:require [clojure.string :as str]))
 
 (defn scrutinize
-  "Check to see if map has scrutiny, if so returns map.
-   Otherwise, the function call result with map, msg, and pred."
-  [m f]
-  (if (:scrutiny m)
+  "Returns the map or map with scrutiny message based on result."
+  [m k pred msg]
+  (if pred
     m
-    (let [{:keys [msg pred]} (f)]
-      (result m msg pred))))
+    (update-in m [:scrutiny] assoc k msg)))
+
+(defn msg
+  "Using this fn because calling name on nil throws an exception."
+  [& input]
+  (str/replace (apply str input) #":" ""))
 
 (defn- present-helper
-  "Checks to see if k is not in map.  Uses a custom false key."
+  "Checks if key is not in map. Uses custom return key 
+   to make sure the key is missing, and not just false or nil."
   [m k]
-  (scrutinize m #(hash-map :msg (str k " is not present")
-                           :pred (not= :scr-false (get m k :scr-false)))))
+  (let [pred (not= :scr-false (get m k :scr-false))
+        msg (msg k " must be present")]
+    (scrutinize m k pred msg)))
 
 (defn present [m & kys]
   (reduce present-helper m kys))
 
 (defn- required-helper
-  "Checks if k is empty or nil - meant for strings"
+  "Check to see if key in map is empty or nil.
+   - extra check for numbers in order to avoid exception on empty?"
   [m k]
-  (scrutinize m #(hash-map :msg (str k " was empty or nil")
-                           :pred (not (or (empty? (k m))
-                                          (nil? (k m)))))))
+  (let [res (k m)
+        pred (or (number? res)
+                 (not (or (empty? res) (nil? res))))
+        msg (msg k " is required")]
+    (scrutinize m k pred msg)))
 
 (defn required [m & kys]
   (reduce required-helper m kys))
 
 (defn- confirmation-helper
-  "Check to see if key equals another key"
+  "Check to see if key equals another key.
+   Failure will be reported to the first k in the pair"
   [m [k k2]]
-  (scrutinize m #(hash-map :msg (str k " and " k2 " do not match")
-                           :pred (= (k m) (k2 m)))))
+  (let [pred (= (k m) (k2 m))
+        msg (msg k " and " k2 " do not match")]
+    (scrutinize m k pred msg)))
 
 (defn confirmation [m & kys]
   {:pre [(even? (count kys))]}
@@ -48,35 +52,20 @@
 (defn- length-helper
   "Checks result of applying f to value."
   [f m k]
-  (scrutinize m #(hash-map :msg (str k " failed length function")
-                           :pred (f (k m)))))
+  (let [pred (f (k m))
+        msg (msg k " does not meet length requirements")]
+    (scrutinize m k pred msg)))
 
 (defn length [m f & kys]
   (reduce (partial length-helper f) m kys))
 
-(defn- inclusion-helper
-  "Checks to see if key returns a truthy value from function"
-  [f m k]
-  (scrutinize m #(hash-map :msg (str k " failed inclusion function")
-                           :pred (f (k m)))))
-
-(defn inclusion [m f & kys]
-  (reduce (partial inclusion-helper f) m kys))
-
-(defn- exclusion-helper
-  "Checks to see if key returns a truthy value from function"
-  [f m k]
-  (scrutinize m #(hash-map :msg (str k " failed exclusion function")
-                           :pred (not (f (k m))))))
-
-(defn exclusion [m f & kys]
-  (reduce (partial exclusion-helper f) m kys))
-
 (defn- match-helper
-  "Checks to see if regex expression finds anything."
+  "Checks to see if regex expression finds anything.
+    -turns value into a string"
   [regex m k]
-  (scrutinize m #(hash-map :msg (str k " failed match function")
-                           :pred (re-find regex (str (k m))))))
+  (let [pred (re-find regex (str (k m)))
+        msg (msg k " does not meet requirements")]
+    (scrutinize m k pred msg)))
 
 (defn match [m regex & kys]
   (reduce (partial match-helper regex) m kys))
@@ -84,10 +73,12 @@
 (defn- custom-helper
   "Checks to see if function returns a truthy value.
    Also applies custom function message."
-  [msg f m k]
-  (scrutinize m #(hash-map :msg msg
-                           :pred (f (m k)))))
+  [f message m k]
+  (let [res (k m)
+        pred (f res)
+        msg  (msg (format message k))]
+    (scrutinize m k pred msg)))
 
 (defn custom
   [m f msg & kys]
-  (reduce (partial custom-helper msg f) m kys))
+  (reduce (partial custom-helper f msg) m kys))
