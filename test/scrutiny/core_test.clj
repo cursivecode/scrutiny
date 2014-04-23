3af03dd1c633bd6b620d98d7e942899c0c78b75f
cursivecode <cursivecode@gmail.com>
2014-04-23 02:42:01 -0400

    Change tests

--- test/scrutiny/core_test.clj
+++ test/scrutiny/core_test.clj
@@ -7,146 +7,82 @@
    :password "abcdef"
    :confirmation "abcdef"
    :gender "male"
-   :middle-name ""
+   :init "jd"
    :age 45
-   :birthday nil
    :nickname "joe dirt"})
 
-(def test-true #(hash-map :msg "success" :pred true))
-(def test-false #(hash-map :msg "fail" :pred false))
-
-(deftest result-test
-  (testing "true validation"
-    (is (= test-map (result test-map "bad" true))
-        "result should equal original map")
-
-    (is (= nil (:scrutiny (result test-map "bad" true)))
-        "scrutiny key should be nil"))
-
-  (testing "false validation"
-    (is (not= test-map (result test-map "bad" false))
-        "result should not equal original map")
-
-    (is (= "bad" (:scrutiny (result test-map "bad" false)))
-        "scrutiny key should be bad")
-
-    (is (= (assoc test-map :scrutiny "bad")
-           (result test-map "bad" false))
-        "map should be the same except for scrutiny key")))
-
 (deftest scrutinize-test
-  (testing "scrutiny key"
-    (is (= test-map (scrutinize test-map test-true))
-        "result should equal original map")
+  (testing "true predicate"
+    (is (= test-map (scrutinize test-map :username true "bad")))
+    (is (= nil (:scrutiny (scrutinize test-map :username true "bad")))))
 
-    (is (= nil (:scrutiny (scrutinize test-map test-true)))
-        "scrutiny key should be nil"))
-
-  (testing "No scrutiny key "
-    (is (not= test-map (scrutinize test-map test-false))
-        "result should not equal original map")
-
-    (is (= "fail" (:scrutiny (scrutinize test-map test-false)))
-        "return custom msg on failed message and false predicate")
-
-    (is (= (assoc test-map :scrutiny "fail")
-           (scrutinize test-map test-false))
-        "map should be the same except for scrutiny key")))
+  (testing "false predicate"
+    (is (not= test-map (scrutinize test-map :age false "bad")))
+    (is (= {:age "bad"} (:scrutiny (scrutinize test-map :age false "bad"))))
+    (is (= test-map
+           (dissoc (scrutinize test-map :age false "bad") :scrutiny)))))
 
 (deftest present-test
-  (testing "key(s) - present"
-    (is (= test-map (present test-map :username :gender))
-        "result should equal original map"))
+  (testing "present"
+    (is (= test-map (present test-map :username :gender)))
+    (is (= nil (:scrutiny (present test-map :age :gender)))))
 
-  (testing "key(s) - FAIL present"
-    (is (not= test-map (present test-map :username :user))
-        "should not equal original map")
-
-    (is (= ":user is not present"
-           (:scrutiny (present test-map :user :not-here)))
-        "should fail on first failure")))
+  (testing "fail present"
+    (is (not= test-map (present test-map :username :user)))
+    (is (= {:user "user must be present"
+            :not-here "not-here must be present"}
+           (:scrutiny (present test-map :user :not-here :username))))))
 
 (deftest required-test
-  (testing "key(s) - required"
-    (is (= test-map (required test-map :username :gender))
-        "result should equal original map"))
+  (testing "required"
+    (is (= test-map (required test-map :username :age))))
 
-  (testing "key(s) - fail required"
-    (is (not= test-map (required test-map :birthday))
-        "fail on nil")
-
-    (is (not= test-map (required test-map :middle-name))
-        "fail on empty string")
-
-    (is (= ":birthday was empty or nil"
-           (:scrutiny (required test-map :birthday :middle-name))))))
+  (testing "fail required"
+    (is (not= test-map (required (assoc test-map :init nil))))
+    (is (not= test-map (required (assoc test-map :init "") :init)))
+    (is (= {:nada "nada is required"}
+           (:scrutiny (required test-map :username :nada))))))
 
 (deftest confirmation-test
-  (testing "key(s) - confirmation"
+  (testing "confirmation"
     (is (= test-map (confirmation test-map
-                                  :confirmation :password :age :age))
-        "result should equal original map"))
+                                  :confirmation :password :age :age))))
 
-  (testing "key(s) - fail confirmation"
-    (is (not= test-map (confirmation test-map
-                                     :age :age :password :username))
-        "should not equal original map")
-
-    (is (= ":age and :gender do not match"
+  (testing "fail confirmation"
+    (is (= {:age "age and gender do not match"
+            :password "password and username do not match"}
            (:scrutiny (confirmation test-map
-                                    :age :gender :password :username)))
-        "should fail with msg on first failure")))
+                                    :age :gender :password :username))))))
 
 (deftest length-test
-  (testing "key(s) - length"
-    (is (= test-map (length test-map #(> % 40) :age))
-        "result should equal orignal map"))
+  (testing "length"
+    (is (= test-map (length test-map #(> % 40) :age)))
+    (is (= test-map (length test-map #(>= (count %) 6) :password))))
 
-  (testing "key(s) - fail length"
-    (is (= ":age failed length function"
-           (:scrutiny (length test-map #(> % 45) :age)))
-        "should fail with message")))
-
-(deftest inclusion-test
-  (testing "key(s) - inclusion"
-    (is (= test-map (inclusion test-map #(some #{%} (range 18 46)) :age))
-        "result should equal original map"))
-
-  (testing "key(s) - fail inclusion"
-    (is (= ":age failed inclusion function"
-           (:scrutiny (inclusion test-map #(some #{%} (range 50 60)) :age)))
-        "should fail with message")))
-
-(deftest exclusion-test
-  (testing "key(s) - exclusion"
-    (is (= test-map (exclusion test-map #(some #{%} (range 50 70)) :age))
-        "result should equal original map"))
-
-  (testing "key(s) - fail exclusion"
-    (is (= ":age failed exclusion function"
-           (:scrutiny (exclusion test-map #(some #{%} (range 18 46)) :age)))
-        "should fail with message")))
+  (testing "fail length"
+    (is (= {:age "age does not meet length requirements"}
+           (:scrutiny (length test-map #(> % 45) :age))))))
 
 (deftest match-test
-  (testing "key(s) - match"
-    (is (= test-map (match test-map #"[a-zA-Z]" :username :gender))
-        "result should equal original map"))
+  (testing "match"
+    (is (= test-map (match test-map #"[a-zA-Z]" :username :gender))))
 
-  (testing "key(s) - fail match"
-    (is (= ":username failed match function"
-           (:scrutiny (match test-map #"\d+" :username :gender)))
-        "should fail with message")))
+  (testing "fail match"
+    (is (= {:username "username does not meet requirements"
+            :gender "gender does not meet requirements"}
+           (:scrutiny (match test-map #"\d+" :username :gender))))))
 
 (deftest custom-test
-  (testing "key(s) - custom"
-    (is (= test-map (custom test-map #(string? %) "" :gender))
-        "result should equal original map"))
+  (testing "custom"
+    (is (= test-map (custom test-map #(string? %) "" :gender)))
+    (is (= test-map
+           (custom test-map #(some #{%} ["admin" "user"]) :username))))
 
-  (testing "key(s) - fail custom"
-    (is (= "wrong"
-           (:scrutiny (custom test-map #(number? %) "wrong"
-                              :username :gender)))
-        "should fail with message")))
+  (testing "fail custom"
+    (is (= {:username "username wrong username"
+            :gender "gender wrong gender"}
+           (:scrutiny (custom test-map #(number? %)
+                              "%1$s wrong %1$s" :username :gender))))))
 
 (deftest scrutiny
   (testing "no failures"
@@ -154,8 +90,6 @@
            (-> test-map
                (present :password :gender :confirmation)
                (confirmation :password :confirmation :username :username)
-               (inclusion #(some #{%} (range 18 46)) :age)
-               (exclusion #(some #{%} ["admin" "administrator"]) :username)
                (match #"[a-zA-Z]" :password)
                (length #(and (< (count %) 15)
                              (> (count %) 2)) :username :password)
@@ -163,10 +97,10 @@
                (custom #(number? %) "Age must be a number" :age)))))
 
   (testing "failure"
-    (is (= ":password failed match function"
+    (is (= {:password "password does not meet requirements"
+            :nada "nada must be present"}
            (:scrutiny (-> test-map
                           (match #"\d+" :password)
-                          (match #"\d+" :username))))
-        "returns first failure")))
+                          (present :nada :username)))))))
 
 
